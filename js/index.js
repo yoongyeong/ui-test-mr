dbeab74862a30e8d82ead243d6211987f1642df4
yoongyeong <yungyung131331@yahoo.com>
2019-10-03 17:39:39 +0700

    Update style.css, and 7172 more files...

--- js/index.js
+++ js/index.js
@@ -12,3 +12,17 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+function openWarning(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
