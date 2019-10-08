417ade1654138cebb14d3c10567fcf80b3a439ae
yoongyeong <yungyung131331@yahoo.com>
2019-10-08 18:00:45 +0700

    Update style.css, index.html, and 9 more files...

---
+++ js/data.js
@@ -0,0 +1,16 @@
+var formItem = [{
+  "ID": 1,
+  "Title": "CPPT",
+  "FormNotif": "(2 need verification)",
+  "FormContent": "<p> Lorem ipsum </p>"
+}, {
+  "ID": 2,
+  "Title": "Assessment Awal Perawat",
+  "FormNotif": "",
+  "FormContent": "<p> Lorem ipsum 2</p>"
+}, {
+  "ID": 3,
+  "Title": "Assessment Awal Dokter",
+  "FormNotif": "",
+  "FormContent": "<p> Lorem ipsum 3</p>"
+}];
--- js/index.js
+++ js/index.js
@@ -1,30 +1,56 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+// var acc = document.getElementsByClassName("accordion");
+// var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active-accordion");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     } 
+//   });
+// }
 
+
+// ACCORDION
+$(document).ready(function(){
+  $(".accordion-container").slideUp();
+  $(".accordion-header").click(function(){
+    $(this).next(".accordion-container").slideToggle(200);
+    $(this).find(".icon").toggleClass("rotate");
+    $(this)
+  })
+});
+
+// $(document).ready(function($){
+//   var panels = $('.accordion-content').hide();
+//   panels.first().show();
+  
+//   $('.accordion-header').click(function() {
+//     var $this = $(this);
+//     panels.slideUp();
+//     $this.parent().next().slideDown();
+    
+//     return false;
+//   });
+// });
+
+
+// TABS
 function openWarning(evt, warningName) {
-  var i, tabcontent, tablinks;
+  var i, tabcontent, tablink;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  tablink = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablink.length; i++) {
+    tablink[i].className = tablink[i].className.replace(" active-tab", "");
   }
   document.getElementById(warningName).style.display = "block";
-  evt.currentTarget.className += " active";
+  evt.currentTarget.className += " active-tab";
 }
 
 // Get the element with id="defaultOpen" and click on it
